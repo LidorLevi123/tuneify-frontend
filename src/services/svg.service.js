2386ee2950caf4a470ad35d1980dc8928d9e9e13
Yaron Levy <ron5054@gmail.com>
2023-07-26 14:49:50 +0300

    Svg refactor

--- src/services/svg.service.js
+++ src/services/svg.service.js
@@ -46,6 +46,13 @@ const TunifySvgs = {
       d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 0 1 0 2 0V3a1 1 0 0 0-1-1z"
       fill="#b3b3af">
   </path>
+</svg>`,
+
+  play: `<svg role="img" height="16" width="16" aria-hidden="true"
+  viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq">
+  <path
+      d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z">
+  </path>
 </svg>`
 }
 
