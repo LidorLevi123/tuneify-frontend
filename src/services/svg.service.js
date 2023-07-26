947dfcfbdc56b5ac6b31c299cba5804c137829c2
Nitzan <prnitzan@gmail.com>
2023-07-26 20:27:01 +0300

    Fix svg

--- src/services/svg.service.js
+++ src/services/svg.service.js
@@ -66,7 +66,11 @@ const TunifySvgs = {
   <path
       d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z">
   </path>
-</svg>`
+</svg>`,
+  pause: `<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon"
+  class="Svg-sc-ytk21e-0 haNxPq"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0
+  .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0
+  .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>`
 }
 
 function getSvg(iconName) {
