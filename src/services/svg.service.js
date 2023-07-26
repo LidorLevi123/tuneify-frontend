8d60428e890e91db3c602fd9c41a9fadb8b494f1
Yaron Levy <ron5054@gmail.com>
2023-07-26 14:40:51 +0300

    Add search input + svg refactor

--- src/services/svg.service.js
+++ src/services/svg.service.js
@@ -32,9 +32,21 @@ const TunifySvgs = {
   </path>
   </svg>`,
 
-  createList: `<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq"><path d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"></path></svg>`,
+  createList: `<svg role="img" height="16"
+  width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon">
+  <path class="plus-icon"
+      d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"
+      fill="#b3b3af">
+  </path>
+</svg>`,
 
- 
+  collapse: ` <svg role="img" height="24" width="24" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon"
+  class="Svg-sc-ytk21e-0 haNxPq">
+  <path class="icon"
+      d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 0 1 0 2 0V3a1 1 0 0 0-1-1z"
+      fill="#b3b3af">
+  </path>
+</svg>`
 }
 
 function getSvg(iconName) {
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,14 +1,35 @@
 <template>
-    <section class="search-page">
-      <button>SEARCH PAGE HERE</button>
-    </section>
-  </template>
+  <section class="search-page">
+    <label for="search">
+      <span class="material-symbols-outlined">search</span>
+      <input id="search" v-model="filterBy.txt" @input="onSetFilterBy" type="text"
+        placeholder="What do you want to listen to?">
+      <span class="material-symbols-outlined search-clear" @click="onClearFilter"> close </span>
+    </label>
+  </section>
+</template>
   
-  <script>
-  
-  export default {
-  
-    name: 'SearchPage',
+<script>
+
+export default {
+
+  name: 'SearchPage',
+  data() {
+    return {
+      filterBy: {
+        txt: '',
+      }
+    }
+  },
+  methods: {
+    onSetFilterBy() {
+      this.$emit('filter', this.filterBy)
+    },
+    onClearFilter() {
+      this.filterBy.txt = ''
+    }
   }
-  </script>
+
+}
+</script>
   
