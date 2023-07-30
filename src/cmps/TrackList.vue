0600c665ddbe6a089128f5c1164e92ad5beb8939
LidorLevi123 <lidorlevi728@gmail.com>
2023-07-30 14:21:04 +0300

    Bug fix

--- src/cmps/TrackList.vue
+++ src/cmps/TrackList.vue
@@ -11,6 +11,17 @@
         <ul v-if="station?.tracks" class="clean-list">
             <li v-for="(track, idx) in station.tracks" :key="track.id" @click="trackClicked(idx)">
                 <TrackPreview :track="track" :trackIdx="idx" />
+                <section class="track-actions">
+                    <!-- <span class="material-symbols-outlined">favorite</span> -->
+                    <!-- <button class="small-like-dis" @click="toggleLike(trackIdx - 1)">
+                    <span v-if="this.isLiked" v-icon="`smallLikeEna`"></span>
+                    <span v-else v-icon="`smallLikeDis`"></span>
+                    </button> -->
+                    <!-- <button class="small-like-dis" v-if="title.isHovered && !track.isLiked">
+                    </button>
+                    <button class="small-like-ena" v-if="title.isHovered && track.isLiked">
+                    </button> -->
+                </section>
             </li>
         </ul>
     </section>
