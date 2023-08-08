aabbb6b39c93bbca28f3f034ae1df091b4b05ed2
LidorLevi123 <lidorlevi728@gmail.com>
2023-08-08 13:37:37 +0300

    Clean code at yt service

--- src/services/yt.service.js
+++ src/services/yt.service.js
@@ -8,7 +8,8 @@ async function queryYT(term) {
     // const API_KEY = 'AIzaSyAFo_70utuVUzF4r0LOkUY-Ic3D_r42q4Y'
     // const API_KEY = 'AIzaSyBK8Ic74OFOgiOBsNLq1IdL-KiohmWxSWc'
     // const API_KEY = 'AIzaSyDs5x70KoJf284KXR-EZbtFcZD4p0tOdvQ'
-    const API_KEY = 'AIzaSyCp8KMTEjR9frWUGpSnc8Cw5cLVe7wRRDM'
+    // const API_KEY = 'AIzaSyCp8KMTEjR9frWUGpSnc8Cw5cLVe7wRRDM'
+    const API_KEY = 'AIzaSyBSmi6bWbWDWTLM0c2Vnd38PhNqNG7Y7H0'
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${term}`
 
     try {
