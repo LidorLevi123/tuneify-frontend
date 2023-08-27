6a6390735af9031aff6ae1b9720435b0a891c5c8
Yaron Levy <ron5054@gmail.com>
2023-08-27 03:04:06 +0300

    Move youtube api logic to the backend

--- src/services/yt.service.js
+++ src/services/yt.service.js
@@ -1,18 +1,20 @@
+import { httpService } from './http.service'
 
 export const ytService = {
     queryYT
 }
 
 async function queryYT(term) {
-    const API_KEY = 'AIzaSyCp8KMTEjR9frWUGpSnc8Cw5cLVe7wRRDM'
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${term}`
-
     try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data.items[0].id.videoId
-
-    } catch (error) {
-        console.error('Error fetching data:', error)
+        const youtubeId = await httpService.get('youtube/', term)
+        return youtubeId
     }
+    catch (error) {
+        console.log(error)
+        throw new Error
+    }
+
 }
+
+
+
