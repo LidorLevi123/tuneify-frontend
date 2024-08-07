a1b31039dae09c92fba475c7d07aebb126d454bc
Yaron Levy <ron5054@gmail.com>
2024-08-07 14:00:14 +0300

    Fix bug and design

--- src/cmps/ArtistData.vue
+++ src/cmps/ArtistData.vue
@@ -11,15 +11,15 @@
                 <span class="followers-number">{{ artistFollowers.toLocaleString("en-US") }}</span>
                 <span class="followers-text">Followers</span>
                 <section class="socials" v-if="artistSocials">
-                    <a :href="artistSocials.facebook[0].url" target="_blank">
+                    <a  v-if="artistSocials.facebook" :href="artistSocials.facebook[0].url" target="_blank">
                         <span v-icon="`facebook`"></span>
                         <span>Facebook</span>
                     </a>
-                    <a :href="artistSocials.instagram[0].url" target="_blank">
+                    <a v-if="artistSocials.instagram" :href="artistSocials.instagram[0].url" target="_blank">
                         <span v-icon="`instagram`"></span>
                         <span>Instagram</span>
                     </a>
-                    <a :href="artistSocials.twitter[0].url" target="_blank">
+                    <a v-if="artistSocials.twitter" :href="artistSocials.twitter[0].url" target="_blank">
                         <span v-icon="`twitter`"></span>
                         <span>Twitter</span>
                     </a>
